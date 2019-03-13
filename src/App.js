d7cdcc1429bb7e7a68ac99da9ff3fcd7111aebef
christopher mwangi <chrischrismwangi@gmail.com>
2019-03-13 18:50:31 +0300

    edited app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> bye there.
+            Edit <code>src/App.js</code> welcome back.
           </p>
           <a
             className="App-link"
